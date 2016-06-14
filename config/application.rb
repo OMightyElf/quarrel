c38ac53b8e94e93b0600d3227620e70c36f4106d
Adele Yu <fhead17@gmail.com>
2016-06-15 01:02:04 +0800

    add some gems and unsplash for background photos

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,12 @@ module Quarrel
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    Unsplash.configure do |config|    
+      config.application_id     = ENV['UNSPLASH_APPLICATION_ID']    
+      config.application_secret = ENV['UNSPLASH_SECRET']
+      config.application_redirect_uri = 'https://quarrel.herokuapp.com'
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
